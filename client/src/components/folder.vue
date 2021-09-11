5740a93394da4b63a998d82dbce5644ab1f41288
benediktengel <ben.gel@web.de>
2021-09-11 20:35:18 +0200

    feat(download): download a zipped version of the folder is now possible

--- client/src/components/folder.vue
+++ client/src/components/folder.vue
@@ -14,6 +14,15 @@
           <span class="sr-only">Show folder information</span>
           <InformationCircleIcon class="flex-shrink-0 w-5 h-5 ml-1 flex-sh text-cda-accent" />
         </button>
+        <a 
+          href="#"
+          @click.prevent="download(folderprops)"
+          download
+          :disabled="downloadLoading"
+        >
+          <span class="sr-only">Download folder {{folderprops.name}}</span>
+          <DownloadIcon class="flex-shrink-0 w-5 h-5 ml-1 flex-sh text-cda-accent" />
+        </a>
       </div>
       <ul>
         <li v-for="item in folderprops.includes" class="pt-2" v-if="active">
@@ -31,8 +40,9 @@
 </template>
 
 <script>
+import { getCurrentInstance } from "@vue/runtime-core"
 import file from "./file.vue";
-import { FolderIcon, InformationCircleIcon } from "@heroicons/vue/solid";
+import { FolderIcon, InformationCircleIcon, DownloadIcon } from "@heroicons/vue/solid";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/vue/outline";
 import { ref } from "vue";
 
@@ -49,14 +59,38 @@ export default {
     ChevronDownIcon,
     ChevronUpIcon,
     InformationCircleIcon,
+    DownloadIcon
   },
   emits: ["file-clicked"],
   setup() {
+    const axios = getCurrentInstance().appContext.config.globalProperties.axios;
     let active = ref(false);
+    let downloadLoading = ref(false);
     function toggleActive() {
       active.value = !active.value;
     }
-    return { active, toggleActive };
+    async function download(prop){
+      downloadLoading.value = true;
+      axios.post(
+        import.meta.env.VITE_APP_SERVER + "/download", 
+        { filepath: prop.path }, 
+        {responseType: 'arraybuffer'}
+        )
+        .then(response => {
+          //create blob with response
+          let blob = new Blob([response.data], { type: 'application/zip' }),
+          url = window.URL.createObjectURL(blob)
+          // create a-tag / taken from: https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
+          var a = document.createElement("a");
+          a.href = url;
+          a.download = `${prop.name}.zip`;
+          document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+          a.click();
+          a.remove();
+        })
+       downloadLoading.value = false;
+    }
+    return { active, toggleActive, download, downloadLoading };
   },
 };
 </script>

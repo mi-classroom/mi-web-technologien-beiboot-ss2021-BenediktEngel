f635550485832e77becb0e4487e2009e2664a022
benediktengel <ben.gel@web.de>
2021-09-12 19:38:40 +0200

    refactor(json-viewer): moved files and refactored for new design

---
+++ client/src/components/content/json/Bracket.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>
+    <span v-if="isObject(element) && opening" :class="color" class="font-code">{</span>
+    <span v-if="isArray(element) && opening" :class="color" class="font-code">[</span>
+    <span v-if="isObject(element) && !opening" :class="color" class="font-code">},</span>
+    <span v-if="isArray(element) && !opening" :class="color" class="font-code">],</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    active: Boolean,
+    element: [String, Number, Object],
+    opening: Boolean, 
+    color: String
+  },
+  components: {},
+  setup(props) {
+     function isObject(el) {
+      return typeof el === "object" && !Array.isArray(el);
+    }
+    function isArray(el) {
+      return Array.isArray(el);
+    }
+    return {isObject, isArray};
+  },
+};
+</script>
--- client/src/components/content/json/JsonViewer.vue
+++ client/src/components/content/json/JsonViewer.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <h2 class="absolute w-full py-6 pl-6 text-2xl text-cda-light bg-cda-darkest">
-      Folderdata for
-      <span class="font-mono text-lg text-cda-accent">
-        {{ folderData.folder }}
-      </span>
-    </h2>
-    <div class="h-screen pt-20 pb-32 overflow-y-scroll">
+    <h2 class="sr-only">Folderdata for {{ folderData.folder }}</h2>
+    <div class="h-screen pt-10 pb-32 mr-2 overflow-y-scroll scrollbar-thumb-cda-light scrollbar-thin">
       <ul v-if="folderData.data" class="px-8">
-        <li v-for="(item, key) in folderData.data.imageStack">
-          <dataList :element="item" :name="key" open="true" />
+        <span class="material-icons text-cda-lightest transform rotate-90 align-middle"> arrow_right </span>
+        <span class="text-cda-accent font-code">{</span>
+        <li v-for="(item, key) in folderData.data" class="flex">
+          <ViewerItem
+            :element="item"
+            :name="key"
+            :depth="0"
+            class="ml-7 border-l border-dotted border-cda-lighten_strong"
+          />
         </li>
+        <span class="text-cda-light pl-6 font-code">}</span>
       </ul>
     </div>
   </div>
@@ -19,7 +22,8 @@
 <script>
 import { getCurrentInstance } from "@vue/runtime-core";
 import { ref, watch } from "vue";
-import dataList from "./dataList.vue";
+
+import ViewerItem from "./ViewerItem.vue";
 
 export default {
   props: {
@@ -28,22 +32,26 @@ export default {
       required: true,
     },
   },
-  components: { dataList },
+  components: { ViewerItem },
   setup(props) {
     const axios = getCurrentInstance().appContext.config.globalProperties.axios;
     let folderData = ref([]);
+
     getData(props.path);
+
     watch(
       () => props.path,
       (newPath) => {
         getData(newPath);
       }
     );
+
     function getData(path) {
       axios.post(import.meta.env.VITE_APP_SERVER + "/json", { filepath: path }).then((response) => {
         folderData.value = response.data;
       });
     }
+
     return { folderData };
   },
 };
